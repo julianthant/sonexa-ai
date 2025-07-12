a3a79ee4501d863a9591d7925afd45933ee2ae5c
julianthant <julianthant@gmail.com>
2025-07-11 21:43:06 -0700

    feat(dto): create NotificationSettingsResponse for preference data

---
+++ backend/src/main/java/com/sonexa/backend/dto/settings/NotificationSettingsResponse.java
@@ -0,0 +1,75 @@
+package com.sonexa.backend.dto.settings;
+
+public class NotificationSettingsResponse {
+
+    private boolean emailNotifications;
+    private boolean pushNotifications;
+    private boolean smsNotifications;
+    private boolean voiceNotifications;
+    private boolean marketingEmails;
+    private boolean securityAlerts;
+
+    // Constructors
+    public NotificationSettingsResponse() {
+    }
+
+    public NotificationSettingsResponse(boolean emailNotifications, boolean pushNotifications,
+            boolean smsNotifications, boolean voiceNotifications,
+            boolean marketingEmails, boolean securityAlerts) {
+        this.emailNotifications = emailNotifications;
+        this.pushNotifications = pushNotifications;
+        this.smsNotifications = smsNotifications;
+        this.voiceNotifications = voiceNotifications;
+        this.marketingEmails = marketingEmails;
+        this.securityAlerts = securityAlerts;
+    }
+
+    // Getters and Setters
+    public boolean isEmailNotifications() {
+        return emailNotifications;
+    }
+
+    public void setEmailNotifications(boolean emailNotifications) {
+        this.emailNotifications = emailNotifications;
+    }
+
+    public boolean isPushNotifications() {
+        return pushNotifications;
+    }
+
+    public void setPushNotifications(boolean pushNotifications) {
+        this.pushNotifications = pushNotifications;
+    }
+
+    public boolean isSmsNotifications() {
+        return smsNotifications;
+    }
+
+    public void setSmsNotifications(boolean smsNotifications) {
+        this.smsNotifications = smsNotifications;
+    }
+
+    public boolean isVoiceNotifications() {
+        return voiceNotifications;
+    }
+
+    public void setVoiceNotifications(boolean voiceNotifications) {
+        this.voiceNotifications = voiceNotifications;
+    }
+
+    public boolean isMarketingEmails() {
+        return marketingEmails;
+    }
+
+    public void setMarketingEmails(boolean marketingEmails) {
+        this.marketingEmails = marketingEmails;
+    }
+
+    public boolean isSecurityAlerts() {
+        return securityAlerts;
+    }
+
+    public void setSecurityAlerts(boolean securityAlerts) {
+        this.securityAlerts = securityAlerts;
+    }
+}
